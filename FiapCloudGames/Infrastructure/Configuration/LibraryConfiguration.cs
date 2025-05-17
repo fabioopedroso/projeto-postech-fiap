f47a1cb25bb3df131714e181f409162abd5a7a40
Fabio Pedroso Lucas <fabiopedrosolucas@gmail.com>
2025-05-17 14:29:26 -0300

    Add new migration

--- FiapCloudGames/Infrastructure/Configuration/LibraryConfiguration.cs
+++ FiapCloudGames/Infrastructure/Configuration/LibraryConfiguration.cs
@@ -11,7 +11,6 @@ public class LibraryConfiguration : IEntityTypeConfiguration<Library>
         builder.HasKey(l => l.Id);
         builder.Property(l => l.Id).HasColumnType("INT").UseIdentityColumn();
         builder.Property(l => l.UserId).IsRequired().HasColumnType("INT");
-        builder.Property(l => l.GameId).IsRequired().HasColumnType("INT");
         builder.HasOne(l => l.User)
             .WithOne(u => u.Library)
             .HasForeignKey<Library>(l => l.UserId)
---
+++ FiapCloudGames/Infrastructure/Migrations/20250517172628_UpdateSchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "Library");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameId",
+                table: "Library",
+                type: "INT",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
